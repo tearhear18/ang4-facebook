220f273955fdf88c91ad5c61a1837cd1e0cfe1df
Ramel <tearhear18@gmail.com>
2017-10-24 23:56:39 +0800

    added descrition content body

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PublicComponent } from './public.component';
+
 const routes: Routes = [
-  {path:'public', component: PublicComponent }
+//  {path:'public', component: PublicComponent }
 ];
 
 @NgModule({
