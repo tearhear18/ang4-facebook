422541aa97c87e79a626ee6bb3e689ccd0f842be
Ramel <tearhear18@gmail.com>
2017-10-25 18:51:03 +0800

    dummy content

--- src/app/member/member.module.ts
+++ src/app/member/member.module.ts
@@ -5,12 +5,13 @@ import { MemberRoutingModule } from './member-routing.module';
 import { MemberpageComponent } from './memberpage.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { PostComponent } from './components/body/post/post.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MemberRoutingModule
   ],
-  declarations: [MemberpageComponent, HeaderComponent, BodyComponent]
+  declarations: [MemberpageComponent, HeaderComponent, BodyComponent, PostComponent]
 })
 export class MemberModule { }
